fb6f3a9030caa82022059fd9b97a75a6a1dee3ff
Vijay-6008 <Vijaymk1116@gmail.com>
2026-04-13 16:55:19 +0530

    Delete hello.py

--- hello.py
+++
@@ -1,7 +0,0 @@
-def greet(name):
-    return f"Hello, {name}! Welcome to GitHub."
-
-if __name__ == "__main__":
-    user_name = input("Enter your name: ")
-    message = greet(user_name)
-    print(message)
